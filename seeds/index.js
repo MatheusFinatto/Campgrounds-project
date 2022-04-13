ff95a2f06f9b9d9ed80435b821b0c9ee1e7944d4
Matheus Finatto <matheusfinatto2@gmail.com>
2022-04-13 17:34:52 -0300

    bootstrap styling

--- seeds/index.js
+++ seeds/index.js
@@ -13,14 +13,17 @@ db.once("open", ()=>{
 })
 
 const sample = array => array[Math.floor(Math.random() * array.length)]
-
+const price = Math.floor(Math.random() * 20) + 10 
 const seedDB = async() =>{
     await Campground.deleteMany({});
     for(let i=0; i < 50; i++){
         const random = Math.floor(Math.random() * 1000) + 1
         const camp = new Campground({
             location: `${cities[random].city}, ${cities[random].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            img:"https://source.unsplash.com/collection/483251",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse ipsam quo sapiente totam, doloremque eos consequuntur vero consequatur cumque ab nisi, neque, quis minima temporibus magnam nesciunt ex incidunt architecto.",
+            price
         })
         await camp.save();
     }
